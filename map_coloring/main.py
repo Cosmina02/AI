3369b0b5fbdeaf17cd752e2613d95308708011e2
Iordan Cosmina <58854421+Cosmina02@users.noreply.github.com>
2021-10-25 20:03:06 +0300

    o incercare de fc,inca nu merge

--- map_coloring/main.py
+++ map_coloring/main.py
@@ -1,3 +1,4 @@
+import random
 
 region_list = ['WA', 'SA', 'NT']  # lista cu regiunile
 
@@ -7,10 +8,10 @@ colors_list = [{'R', 'G', 'B'}, {'R', 'G'}, {'G'}]
 
 # o lista cu adiacenta intre regiuni
 region_adjacency = {
-                    'WA': {'SA', 'NT'},
-                    'SA': {'WA', 'NT'},
-                    'NT': {'WA', 'SA'}
-                    }
+    'WA': {'SA', 'NT'},
+    'SA': {'WA', 'NT'},
+    'NT': {'WA', 'SA'}
+}
 
 
 def is_consistent(region1, region2, color1):
@@ -27,3 +28,28 @@ def is_empty(region):
     if len(colors_list[region_list.index(region)]) > 0:
         return False
     return True
+
+
+def forward_checking(r, c):
+    i = 0
+    while is_empty(r[i]) is False:
+        a = random.sample(c[r.index(r[i])], 1)  # ia random o culoare din lista de culori a regiunii i
+        a = ' '.join(a)  # transforma culoarea din lista in string(pentru a putea fi folosita mai departe)
+        remove_color(r[i], a)
+        empty_domain = False
+        for k in range(i + 1, len(r)):
+            for b in c[r.index(r[k])]:
+                if is_consistent(r[i], r[k], a):
+                    remove_color(r[k], b)
+            if is_empty(r[k]):
+                empty_domain = True
+        if empty_domain:
+            a = 1  # asta e random pus -> trebuie reset function(nu stiu inca cum)
+        else:
+            return a
+    return 0
+
+
+if __name__ == '__main__':
+    forward_checking(region_list, colors_list)
+    print(colors_list)
