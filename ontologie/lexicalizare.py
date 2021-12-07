f785603baf6326b066731254d8607be5e264af08
Iordan Cosmina <58854421+Cosmina02@users.noreply.github.com>
2021-12-07 22:41:18 +0200

    pct4

---
+++ ontologie/lexicalizare.py
@@ -0,0 +1,44 @@
+import lightrdf
+
+parser = lightrdf.Parser()
+
+def is_reapeating(words,text):
+    for word in words:
+        if word == text:
+            return True
+    return False
+
+def ontology_parser():
+    words = list()
+    with open("ontology.owl", "rb") as f :
+        for triple in parser.parse(f, format="owl", base_iri=None) :
+            chunk = triple[0].split('/')
+            if is_reapeating(words, chunk[len(chunk)-1]) is False:
+                words.append(chunk[len(chunk)-1])
+
+    return words
+
+def modify_words(text):
+    words = list()
+    for t in text:
+        word = t.replace("_"," ")
+        words.append(word)
+    return words
+
+def find_words_in_file(words):
+    with open("result.txt", 'r', encoding='utf-8') as f:
+        g = open("output.txt", 'w', encoding='utf-8')
+        text = f.read().split('\n')
+        for line in text:
+            for word in words:
+                if line.find(word, 0, len(line))!=-1:
+                    print(line)
+                    g.write(line)
+                    g.write('\n')
+
+
+# words = ontology_parser()
+# words = modify_words(words)
+ceva=['1960','computer']
+find_words_in_file(ceva)
+# print(modify_words(ceva))
--- ontologie/parser.py
+++ ontologie/parser.py
@@ -4,8 +4,14 @@ parser = lightrdf.Parser()
 
 word = input("Please enter a word: ")
 
-with open("ontology.owl", "rb") as f:
-    for triple in parser.parse(f, format="owl", base_iri=None):
-        if triple[0].find(word, 0, len(triple[0])) != -1:
-            if triple[1].find("superTopicOf", 0, len(triple[1])) != -1:
-                print(triple)
+
+
+def parse_ontology(word):
+    with open("ontology.owl", "rb") as f:
+        for triple in parser.parse(f, format="owl", base_iri=None):
+            if triple[0].find(word, 0, len(triple[0])) != -1:
+                if triple[1].find("superTopicOf", 0, len(triple[1])) != -1:
+                    print(triple)
+
+
+parse_ontology(word)
