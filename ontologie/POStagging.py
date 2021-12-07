890a67aba2acbdca9d95b7f008933c9b0098676a
Serithipithy <alexandramosoiu19@gmail.com>
2021-12-07 21:34:31 +0200

    punct 3 done

--- ontologie/POStagging.py
+++ ontologie/POStagging.py
@@ -1,37 +1,47 @@
-import nltk
 from nltk import pos_tag
-from nltk import RegexpParser
-
-def remove_empty_string(text,index):
-    text.pop(index)
-    return text
-
-
-with open("computer-science.txt", 'r', encoding = 'utf-8') as f:
-    g = open("result.txt", 'w')
-    text = f.read().split('\n')
+def file_splitter(text):
     sentences = list()
     for sentence in text:
         if sentence.find('.', 0, len(sentence)) != -1:
             chunk = sentence.split('.')
             for c in chunk:
                 sentences.append(c)
-                # print(c)
-    # print(sentences)
+    return sentences
 
+def generate_file(sentences):
+    sentences_in_file = 0
     for sentence in sentences:
         words = sentence.split()
-        # print(words, "\n##########\n")
         tokens_tag = pos_tag(words)
-        # print(tokens_tag,"\n##########\n")
         poz = 0
-        poz_s1 = 0
-        poz_s2 = 0
-        poz_v = 0
+        poz_s1 = -1
+        poz_s2 = -1
+        poz_v = -1
+        k = 0
         for tuple in tokens_tag:
-            print(tuple)
-            if tuple[1].find('NN', 0, len(tuple[1])) != -1:
+            # print(tuple)
+            if poz_s1 == -1 and tuple[1].find('NN', 0, len(tuple[1])) != -1:
                 poz_s1 = poz
+            elif poz_s1 != -1 and poz_s2 == -1 and poz_v != -1 and tuple[1].find('NN', 0, len(tuple[1])) != -1:
+                poz_s2 = poz
+            elif poz_v == -1 and tuple[1].find('VB', 0, len(tuple[1])) != -1:
+                poz_v = poz
+                k += 1
+            elif poz_s1 != -1 and poz_s2 == -1 and poz_s1 < poz and tuple[1].find('VB', 0, len(tuple[1])) != -1:
+                k += 1
+            poz += 1
+            # print(poz_s1," ",poz_v," ",poz_s2, " ", k)
+        if k == 1 and poz_s1 < poz_v < poz_s2:
+            sentences_in_file += 1
+            g.write(sentence)
+            g.write("\n")
+    return sentences_in_file
+# nltk.download()
+with open("computer-science.txt", 'r', encoding='utf-8') as f:
+    g = open("result.txt", 'w', encoding='utf-8')
+    text = f.read().split('\n')
+    sentences = file_splitter(text)
 
-            ++poz
+    print("Number of sentances in input file :",len(sentences))
 
+    print("Number of sentences in result file :",generate_file(sentences))
